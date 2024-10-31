fc8272bed58864563a4c123baaf9b00ca142f180
Gary Bortosky <garybort@gmail.com>
2024-10-31 16:57:26 -0500

    import from lib

--- apps/greeter/src/app/main.ts
+++ apps/greeter/src/app/main.ts
@@ -1,13 +1,3 @@
-const message = "nothing to see";
-
-export function greet(name: string): string {
-  return `main says: hello to ${name}`;
-}
-export function add(a: number, b: number): number {
-  return a + b;
-}
-export const meaning: { life: number } = {
-  life: 42,
-};
+import { message } from "@pnpm-workspace/shared";
 
 console.log(message);
