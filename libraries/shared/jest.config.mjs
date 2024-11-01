bdf29fe1d76e6ee059dacbd845d82c90b2765f9d
Gary Bortosky <GaryB432@users.noreply.github.com>
2024-11-01 08:53:55 -0500

    format

--- libraries/shared/jest.config.mjs
+++ libraries/shared/jest.config.mjs
@@ -1,34 +1,17 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 import { createDefaultEsmPreset } from "ts-jest";
 
-const defaultEsmPreset = createDefaultEsmPreset()
+const defaultEsmPreset = createDefaultEsmPreset();
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 const config = {
   ...defaultEsmPreset,
-  extensionsToTreatAsEsm: ['.ts'],
-
   coverageProvider: "v8",
-
-  moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
-  },
-
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: { "^(\\.{1,2}/.*)\\.js$": "$1" },
   testMatch: ["**/*.spec.ts"],
-
   testPathIgnorePatterns: ["\\\\node_modules\\\\"],
-
-  transform: {
-    "^.+.tsx?$": ["ts-jest", {}],
-  },
-
-  transformIgnorePatterns: [
-    "\\\\node_modules\\\\",
-  ]
+  transform: { "^.+.tsx?$": ["ts-jest", { useESM: true }] },
+  transformIgnorePatterns: ["\\\\node_modules\\\\"],
 };
 
 export default config;
